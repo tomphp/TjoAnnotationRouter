4b2773544c4da4c28da58714365f662985f2e659
Tom Oram <tom@scl.co.uk>
2013-02-28 11:07:12 +0000

    Fixed paths

--- src/TjoAnnotationRouter/Module.php
+++ src/TjoAnnotationRouter/Module.php
@@ -44,12 +44,12 @@ class Module implements
         return array(
             /*
             'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
+                __DIR__ . '/../../autoload_classmap.php',
             ),
             */
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__,
                 ),
             ),
         );
@@ -60,7 +60,7 @@ class Module implements
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
     /**
